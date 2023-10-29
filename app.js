30f69f1355881a794ee7b44bce279307eb5c785c
Ghaalia <Ghaliaals9@gmail.com>
2023-10-29 18:35:34 +0300

    ..

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const postsRoutes = require('./api/posts/posts.routes');
-const connectDb = require('./database');
+const postsRoutes = require("./api/posts/posts.routes");
+const connectDb = require("./database");
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/posts", postsRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).json("404 ERROR Path NOT Found !!!");
+});
+
+app.use((error, req, res, next) => {
+  res.status(500).json({ message: error.message });
+});
 
 connectDb();
-app.use(express.json());
-app.use('/posts', postsRoutes);
-
 app.listen(8000, () => {
-  console.log('The application is running on localhost:8000');
+  console.log("The application is running on localhost:8000");
 });
